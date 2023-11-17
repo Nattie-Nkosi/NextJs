0f9e99a76d6f19ef368dfa3b9caa11ade4bf591c
Nutty4616 <nkosin361@gmail.com>
2023-11-17 18:00:12 +0200

    Initial commit

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/hero";
+import reliabilityPage from "public/reliability.jpg";
+
+export default function ReliabilityPage() {
+  return (
+    <Hero
+      imgData={reliabilityPage}
+      imgAlt="Welding"
+      title="Super high reliable hosting"
+    />
+  );
+}
